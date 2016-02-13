e7c927af9fa768ce8fdd30edde4fa86759bb92e3
alex <alexander.bollbach@gmail.com>
2016-02-13 11:52:34 -0500

    Initial Commit

---
+++ WavformVizualizer_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavformVizualizer_iOS
+//
+//  Created by alexanderbollbach on 2/13/16.
+//  Copyright © 2016 alexanderbollbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
